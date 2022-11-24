134f5f744c8cd9d65d34d44e0abc7f249ab0facf
Ahmad Aghazadeh PC <ahmad.aghazadeh@outlook.com>
2022-11-25 00:51:50 +0330

    Added binary Gap

--- DemoTest.java
+++ DemoTest.java
@@ -19,7 +19,7 @@ import java.util.Arrays;
 
 public class DemoTest{
     public static void main(String args[]){  
-        int[] givenArray=new int[]{-1,-2 };
+        int[] givenArray=new int[]{1, 3, 6, 4, 1, 2,7,8};
         System.out.println(solution(givenArray));  
     }  
 
@@ -34,6 +34,8 @@ public class DemoTest{
             }else{
                 currentSmallestNumber++;
             }
+           }else if(currentSmallestNumber<A[i]){
+              return currentSmallestNumber;
            }
         }
         return currentSmallestNumber;

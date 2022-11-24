55f4e6d3f2e3660d974f6e75c0fe339fc45bebbf
Ahmad Aghazadeh PC <ahmad.aghazadeh@outlook.com>
2022-11-24 21:58:07 +0330

    Added Demo

---
+++ DemoTest.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class DemoTest{
+    public static void main(String args[]){  
+        int[] givenArray=new int[]{-1,-2 };
+        System.out.println(solution(givenArray));  
+    }  
+
+    public static int solution(int[] A) {
+        
+        Arrays.sort(A);
+        int currentSmallestNumber=1;
+        for (int i = 0; i < A.length; i++) {
+           if(currentSmallestNumber==A[i] && A[i]>0){
+            if(i==A.length-1){
+                return ++currentSmallestNumber;
+            }else{
+                currentSmallestNumber++;
+            }
+           }
+        }
+        return currentSmallestNumber;
+    }
+}

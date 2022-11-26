209b2e33b0e85f819eba9f7b72303c2f53407621
Ahmad Aghazadeh Home <Ahmad.aghazadeh.a@gmail.com>
2022-11-26 17:58:26 +0330

    FirstUnique

--- FirstUnique/FirstUnique.java
+++ FirstUnique/FirstUnique.java
@@ -1,30 +1,24 @@
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class FirstUnique {
     public static void main(String args[]) {
-        int[] givenArray=new int[]{6, 4, 4, 6};
+        int[] givenArray=new int[]{4, 10, 5, 4, 2, 10};
         int number=solution(givenArray);
         System.out.println(number);
     }
 
     public static int solution(int[] A) {
-        int sorted[]=A.clone();
-        Arrays.sort(sorted);
-        for (int i = 0; i < sorted.length; i++) {
-            int count=0;
-            for (int j = 0; j < sorted.length; j++) {
-                if(A[i]==sorted[j]){
-                    count++;
-                    if(count>1){
-                        break;
-                    }                      
-                }     
-            }
-            if(count==1){
-                return A[i];
+        Set<Integer> input = new LinkedHashSet<>();
+        Set<Integer> duplicates = new HashSet<>();
+        for (Integer x : A) {
+            if (!input.add(x)) {
+                duplicates.add(x);
             }
         }
-        return -1;
+        input.removeAll(duplicates);
+        return input.isEmpty() ? -1 : input.iterator().next();
     }
 
 }

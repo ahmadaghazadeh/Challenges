cb1fe1bd3d0b60ed07e830c6cafb1207dd131153
Ahmad Aghazadeh Home <Ahmad.aghazadeh.a@gmail.com>
2022-11-27 10:46:11 +0330

    Added ArrayInversionCount

--- Codibility/ArrayInversionCount/ArrayInversionCount.java
+++ Codibility/ArrayInversionCount/ArrayInversionCount.java
@@ -7,17 +7,17 @@ public class ArrayInversionCount {
     }
 
     private static int solution(int[] A) {
-        int result=-1;
+        int result=0;
         if (A.length>=1000000000)
-           return result;
+           return -1;
 
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 1; j < A.length; j++) {
-                if(A[i]>A[j])
-                   result++;
+        for (int i = 0; i < A.length-1; i++) {
+            for (int j = i+1; j < A.length; j++) {
+                if(A[i]>A[j]){
+                    result++;
+                   }
+                }
             }
-        }
         return result;
     }
- 
 }
